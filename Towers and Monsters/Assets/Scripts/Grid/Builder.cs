6227c485ae908de8eae63a64ec5e313fb6fb034c
Alexandre Sachs <alexandre.sachs@outlook.fr>
2020-04-07 12:45:16 +0200

    Map Generation and towers placement

---
+++ Towers and Monsters/Assets/Scripts/Grid/Builder.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    #region PrivateVariables
+
+    private Camera _camera;
+    private bool _isCameraNotNull;
+
+    private Grid _gridObject;
+
+    #endregion
+
+    #region PublicVariables
+
+    public GameObject tower;
+
+    #endregion
+
+    #region MonoBehavior
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _isCameraNotNull = _camera != null;
+
+        _gridObject = FindObjectOfType<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_isCameraNotNull)
+            {
+                var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out var click))
+                {
+                    PlaceTower(click.point);
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region PrivateFunctions
+
+    private void PlaceTower(Vector3 clickPoint)
+    {
+        var gridSpacingOffset = _gridObject.gridSpacingOffset;
+        
+        if (_gridObject.AppendElementInGrid(clickPoint, tower))
+        {
+            var xCount = Mathf.RoundToInt(clickPoint.x / gridSpacingOffset);
+            var zCount = Mathf.RoundToInt(clickPoint.z / gridSpacingOffset);
+
+            Vector3 result = new Vector3(
+                xCount * gridSpacingOffset,
+                1,
+                zCount * gridSpacingOffset);
+
+            Instantiate(tower).transform.position = result;
+        }
+    }
+
+    #endregion
+}
---
+++ Towers and Monsters/Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    #region PublicVariables
+
+    public int gridSizeX;
+    public int gridSizeZ;
+    
+    public float gridSpacingOffset = 1f;
+
+    public GameObject[] gridCellToPickFrom;
+
+    #endregion
+
+    #region PrivateVariables
+
+    private readonly Vector3 _gridOrigin = Vector3.zero;
+    
+    private GameObject[,] _grid;
+
+    #endregion
+    
+    #region MonoBehavior
+
+    private void Awake()
+    {
+        _grid = new GameObject[gridSizeX, gridSizeZ];
+
+        SpawnGrid();
+    }
+
+    #endregion
+
+    #region PrivateFunctions
+
+    private void SpawnGrid()
+    {
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int z = 0; z < gridSizeZ; z++)
+            {
+                Vector3 spawnPosition = new Vector3(x * gridSpacingOffset, -0.5f, z * gridSpacingOffset) + _gridOrigin;
+                PickAndSpawnGridCase(spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+
+    private void PickAndSpawnGridCase(Vector3 positionToSpawn, Quaternion rotationToSpawn)
+    {
+        int randomIndex = Random.Range(0, gridCellToPickFrom.Length);
+
+        Instantiate(gridCellToPickFrom[randomIndex], positionToSpawn, rotationToSpawn);
+    }
+
+    #endregion
+    
+    #region PublicFunctions
+
+    #region Getter/Setter
+
+    public GameObject[,] GetGrid()
+    {
+        return _grid;
+    }
+
+    #endregion
+    
+    public bool AppendElementInGrid(Vector3 clickPoint, GameObject obj)
+    {
+        var xCount = Mathf.RoundToInt(clickPoint.x / gridSpacingOffset);
+        var zCount = Mathf.RoundToInt(clickPoint.z / gridSpacingOffset);
+
+        if (_grid[xCount, zCount] == null)
+        {
+            _grid[xCount, zCount] = obj;
+            return true;
+        }
+
+        return false;
+    }
+
+    #endregion
+}
