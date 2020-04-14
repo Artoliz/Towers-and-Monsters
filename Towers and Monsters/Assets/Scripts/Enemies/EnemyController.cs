48de1bb79b3baf7d6083f8353cb6e23252831e65
Alexandre Sachs <alexandre.sachs@outlook.fr>
2020-04-14 12:44:51 +0200

    NavMesh agents work for enemies AI, they avoid obstacles at runtime

---
+++ Towers and Monsters/Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    #region PrivateVariables
+
+    private NavMeshAgent _agent;
+
+    #endregion
+
+    #region PublicVariables
+
+    public GameObject destination;
+
+    #endregion
+
+    #region MonoBehavior
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _agent.SetDestination(destination.transform.position);
+    }
+
+    #endregion
+}
