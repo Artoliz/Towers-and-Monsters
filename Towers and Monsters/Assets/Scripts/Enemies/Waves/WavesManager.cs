2f2d8a40414edabc4d39cdd0270dd3426cc32b58
Alexandre Sachs <alexandre.sachs@outlook.fr>
2020-04-16 11:03:20 +0200

    Timer between waves is done, mechanism between waves is done I think, need to test. A variable is telling is we are between waves or not, timings are reset for each wave, wave number and wave weight are modified. Need also to create the algo for the waveWeight.Now I have to set the enemies spawner and link them to the waveWeight

---
+++ Towers and Monsters/Assets/Scripts/Enemies/Waves/WavesManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WavesManager : MonoBehaviour
+{
+    #region PrivateVariables
+
+    private int _waveWeight;
+    
+    private float _timeBetweenWaves;
+    
+    private bool _firstWave = true;
+    private bool _timerBetweenWavesIsRunning;
+
+    #endregion
+
+    #region PublicVariables
+
+    public int waveNumber = 1;
+    
+    public float setTimeBetweenWaves = 60.0f;
+
+    public static bool GameIsBetweenWaves;
+    
+    public Text timeBetweenWavesText;
+
+    #endregion
+
+    #region MonoBehavior
+
+    private void Awake()
+    {
+        SetGame(0, true);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LaunchTimerWhenSceneIsLoaded());
+    }
+
+    private void Update()
+    {
+        if (_timerBetweenWavesIsRunning)
+        {
+            if (_timeBetweenWaves > 0)
+            {
+                _timeBetweenWaves -= Time.unscaledDeltaTime;
+                DisplayTimeBetweenWaves(_timeBetweenWaves);
+            }
+            else
+            {
+                RunWave();
+            }
+        }
+        else if (GameIsBetweenWaves && !_firstWave) //If it's the first wave we don't reset the data
+        {
+            SetNextWave();
+        }
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private IEnumerator LaunchTimerWhenSceneIsLoaded()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        SetTimeBetweenWaves(true, setTimeBetweenWaves);
+    }
+    
+    void DisplayTimeBetweenWaves(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);  
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeBetweenWavesText.text = $"Time before wave {waveNumber} : {minutes:00}:{seconds:00}";
+    }
+
+    private void SetTimeBetweenWaves(bool timerBetweenIsRunning, float timeBetweenWaves)
+    {
+        _timerBetweenWavesIsRunning = timerBetweenIsRunning;
+        _timeBetweenWaves = timeBetweenWaves;
+    }
+
+    private static void SetGame(int timeScale, bool isBetweenWaves)
+    {
+        Time.timeScale = timeScale;
+        GameIsBetweenWaves = isBetweenWaves;
+    }
+
+    private void RunWave()
+    {
+        _firstWave = false;
+        
+        SetGame(1, false);
+        SetTimeBetweenWaves(false, 0);
+    }
+
+    private void SetNextWave()
+    {
+        SetGame(0, true);
+        SetTimeBetweenWaves(true, setTimeBetweenWaves);
+        waveNumber += 1;
+        _waveWeight += waveNumber;
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public int GetWaveWeight()
+    {
+        return _waveWeight;
+    }
+
+    #endregion
+}
