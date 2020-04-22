39ef41ccaeee15ce9fccc14fdd4c21790439476c
Charles Descoust <charles.descoust@epitech.eu>
2020-04-22 17:46:12 +0900

    Worlds --> Forest

--- Towers and Monsters/Assets/Scripts/Base/Base.cs
+++ Towers and Monsters/Assets/Scripts/Base/Base.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class Base : MonoBehaviour
@@ -7,8 +8,21 @@ public class Base : MonoBehaviour
 
     [SerializeField] private int health = 1000;
 
+    [SerializeField] private Transform _health;
+
+    [SerializeField] private Transform _progress;
+
     #endregion
-    
+
+    #region PrivateVariables
+
+    private float _maxSizeX;
+    private int _maxHealth;
+
+    private Vector3 _size;
+
+    #endregion
+
     #region PublicVariables
 
     public static Base instance;
@@ -22,8 +36,20 @@ public class Base : MonoBehaviour
         instance = this;
     }
 
+    private void Start()
+    {
+        _maxHealth = health;
+        _maxSizeX = _progress.transform.localScale.x;
+        _size = _progress.transform.localScale;
+    }
+
+    private void Update()
+    {
+        _health.LookAt(Camera.main.transform);
+    }
+
     #endregion
-    
+
     #region PublicMethods
 
     public void LoseHealth(int damage)
@@ -36,6 +62,12 @@ public class Base : MonoBehaviour
         {
             health -= damage;
         }
+
+        float currentSizeX = (health * _maxSizeX) / _maxHealth;
+        _progress.localScale = new Vector3(currentSizeX, _size.y, _size.z);
+
+        float currentPosX = -((_maxSizeX - currentSizeX) / 2.0f);
+        _progress.localPosition = new Vector3(currentPosX, 0, 0);
     }
 
     #endregion
