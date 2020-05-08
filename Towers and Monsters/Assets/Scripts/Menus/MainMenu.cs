474ff01d3cd44593726485d4cdf1397f5b9962ef
K6PIR <kellian.cipierre@gmail.com>
2020-05-08 13:16:31 +0200

    Main Menus
    Prefab + menu scene

--- Towers and Monsters/Assets/Scripts/Menus/MainMenu.cs
+++ Towers and Monsters/Assets/Scripts/Menus/MainMenu.cs
@@ -1,13 +1,86 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     #region PublicMethods
 
+    public GameObject MainMenuCanvas;
+    public GameObject LevelSelectionCanvas;
+    public GameObject OptionMenuCanvas;
+    public GameObject HighScoreMenuCanvas;
+    public GameObject QuitGameConfirmationMenuCanvas;
+
+    void Awake()
+    {
+        MainMenuCanvas.SetActive(true);
+        LevelSelectionCanvas.SetActive(false);
+        OptionMenuCanvas.SetActive(false);
+//        HighScoreMenuCanvas.SetActive(false);
+        QuitGameConfirmationMenuCanvas.SetActive(false);
+    }
+
     public void LaunchLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
+    }
+
+    public void SelectLevelSelectionMenu()
+    {
+        MainMenuCanvas.SetActive(false);
+        
+        LevelSelectionCanvas.SetActive(true);
+    }
+    
+    public void SelectOptionMenu()
+    {
+        MainMenuCanvas.SetActive(false);
+        
+        OptionMenuCanvas.SetActive(true);
+    }
+
+    public void SelectHighScoreMenu()
+    {
+        MainMenuCanvas.SetActive(false);
+        
+        HighScoreMenuCanvas.SetActive(true);
+    }
+
+    public void SelectMainMenuFromOptionMenu()
+    {
+        OptionMenuCanvas.SetActive(false);
+        
+        MainMenuCanvas.SetActive(true);
+    }
+
+    public void SelectMainMenuFromLevelSelectionMenu()
+    {
+        LevelSelectionCanvas.SetActive(false);
+        
+        MainMenuCanvas.SetActive(true);
+    }
+
+    public void SelectMainMenuFromHighScoreMenu()
+    {
+        HighScoreMenuCanvas.SetActive(false);
+        
+        MainMenuCanvas.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        QuitGameConfirmationMenuCanvas.SetActive(true);
+    }
+    
+    public void QuitGameConfirm()
+    {
+        Application.Quit();
+    }    
+    
+    public void QuitGameCancel()
+    {
+        QuitGameConfirmationMenuCanvas.SetActive(false);
     }
 
     #endregion
---
+++ Towers and Monsters/Assets/Scripts/Menus/MenuController.cs
@@ -0,0 +1,57 @@
+﻿namespace ArtByKandles
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public sealed class MenuController : MonoBehaviour
+	{
+		[SerializeField] private RectTransform _container = null;
+
+		[SerializeField] private Button _next = null;
+		[SerializeField] private Button _previous = null;
+
+		private int _panel;
+
+		private void Start()
+		{
+			if(_container == null || _next == null || _previous == null)
+			{
+				Debug.LogError("Demo scene not configured");
+
+				return;
+			}
+
+			_next.onClick.AddListener(OnNext);
+			_previous.onClick.AddListener(OnPrevious);
+
+			Refresh();		
+		}
+
+		private void Refresh()
+		{
+			for (int i = 0; i < _container.childCount; ++i)
+				_container.GetChild(i).gameObject.SetActive(i == _panel); 
+		}
+
+		private void OnNext()
+		{
+			_panel = Mathf.Clamp(_panel + 1, 0, _container.childCount - 1);
+
+			Refresh();
+		}
+
+		private void OnPrevious()
+		{
+			_panel = Mathf.Clamp(_panel - 1, 0, _container.childCount - 1);
+
+			Refresh();
+		}
+		
+		private void Update()
+		{
+		
+		}
+	}
+}
