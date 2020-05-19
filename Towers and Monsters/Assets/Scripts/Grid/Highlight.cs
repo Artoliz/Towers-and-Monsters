216e1e4b64a0d402b56dedb1bf3ff2336d3c028b
Charles Descoust <charles.descoust@epitech.eu>
2020-05-19 19:17:45 +0900

    Update Highlights.cs

--- Towers and Monsters/Assets/Scripts/Grid/Highlight.cs
+++ Towers and Monsters/Assets/Scripts/Grid/Highlight.cs
@@ -3,24 +3,26 @@
 public class Highlight : MonoBehaviour
 {
     private Renderer _renderer;
+    private Transform _pivot;
 
     private void Awake()
     {
         _renderer = GetComponent<Renderer>();
+        _pivot = transform.Find("Pivot");
     }
 
     private void OnMouseEnter()
     {
         if (!WavesManager.GameIsFinished && !PauseMenu.GameIsPaused)
         {
-            if (Builder.isBuilding && !Grid.Instance.IsPositionBlocked(transform.position))
+            if (Builder.isBuilding && !Grid.Instance.IsPositionBlocked(_pivot.position))
                 _renderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
         }
     }
 
     private void OnMouseExit()
     {
-        if (!Grid.Instance.IsPositionBlocked(transform.position))
+        if (!Grid.Instance.IsPositionBlocked(_pivot.position))
             _renderer.material.shader = Shader.Find("Diffuse");
     }
 }
