8eea0c2fdefa5acf5d8b863d0e5311b768635114
Alexandre Sachs <alexandre.sachs@outlook.fr>
2020-06-05 15:42:50 +0200

    Create panels + titles + switch scriptfor the tutorial

---
+++ Towers and Monsters/Assets/Scripts/Menus/Tutorial/TutorialSwitchPanels.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSwitchPanels : MonoBehaviour
+{
+    #region SerializableVariables
+
+    [SerializeField] private GameObject tutorialTitle;
+    [SerializeField] private GameObject tutorialButtons;
+
+    [SerializeField] private List<GameObject> tutorialPanels;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    private void OnEnable()
+    {
+        DisableAllPanels();
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private void DisableAllPanels()
+    {
+        tutorialTitle.SetActive(true);
+        tutorialButtons.SetActive(true);
+
+        foreach (var panel in tutorialPanels)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void EnablePanelByName(string panelName)
+    {
+        tutorialTitle.SetActive(false);
+        tutorialButtons.SetActive(false);
+
+        foreach (var panel in tutorialPanels)
+        {
+            panel.SetActive(panel.name == panelName);
+        }
+    }
+
+    #endregion
+}
