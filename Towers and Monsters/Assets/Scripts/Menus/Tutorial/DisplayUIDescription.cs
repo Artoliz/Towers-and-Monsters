a4ebee6728a87f8559740422fb280e224ca45c8e
Alexandre Sachs <alexandre.sachs@outlook.fr>
2020-06-06 22:17:55 +0200

    Finish tutorial UI

---
+++ Towers and Monsters/Assets/Scripts/Menus/Tutorial/DisplayUIDescription.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DisplayUIDescription : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    #region SerializableVariables
+
+    [SerializeField] private GameObject description;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        description.SetActive(false);
+    }
+
+    #endregion
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        description.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        description.SetActive(false);
+    }
+}
