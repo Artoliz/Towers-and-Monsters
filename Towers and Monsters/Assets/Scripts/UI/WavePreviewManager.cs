b7b0f8dbcae9882bdbdc4a5aaba1fbbd559e94bb
K6PIR <kellian.cipierre@gmail.com>
2020-06-08 16:31:46 +0200

    add first implementation of wavePreview in human Valley

---
+++ Towers and Monsters/Assets/Scripts/UI/WavePreviewManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WavePreviewManager : MonoBehaviour
+{
+    
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Open()
+    {
+        animator.SetTrigger("open");
+    }
+
+    public void Close()
+    {
+        animator.SetTrigger("close");
+        
+    }
+}
