57a59502809b068eae74385cc6a118a6a327ab76
Alexandre Sachs <alexandre.sachs@outlook.fr>
2020-06-17 15:02:38 +0200

    Update tuto with wave preview

--- Towers and Monsters/Assets/Scripts/Menus/Tutorial/DisplayUIDescription.cs
+++ Towers and Monsters/Assets/Scripts/Menus/Tutorial/DisplayUIDescription.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DisplayUIDescription : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     #region SerializableVariables
 
     [SerializeField] private GameObject description;
+    
+    [SerializeField] private Image uiInGame;
+    [SerializeField] private Sprite mainImageUi;
+    [SerializeField] private Sprite newImageUi;
 
     #endregion
 
@@ -21,10 +26,20 @@ public class DisplayUIDescription : MonoBehaviour, IPointerEnterHandler, IPointe
     public void OnPointerEnter(PointerEventData eventData)
     {
         description.SetActive(true);
+
+        if (uiInGame != null)
+        {
+            uiInGame.sprite = newImageUi;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         description.SetActive(false);
+        
+        if (uiInGame != null)
+        {
+            uiInGame.sprite = mainImageUi;
+        }
     }
 }
