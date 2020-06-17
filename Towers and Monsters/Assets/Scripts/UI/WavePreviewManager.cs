beee3c7bd975ec223766781e54e71087ef507dfa
K6PIR <kellian.cipierre@gmail.com>
2020-06-17 13:09:07 +0200

    fix wave preview

--- Towers and Monsters/Assets/Scripts/UI/WavePreviewManager.cs
+++ Towers and Monsters/Assets/Scripts/UI/WavePreviewManager.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class WavePreviewManager : MonoBehaviour
 {
+    #region Public Variables
+
+    public float moduleSize = 175.0f;
+    public float offset = -15.0f;
     
-    public Animator animator;
+    #endregion
+
     
-    // Start is called before the first frame update
+    #region Serializable Variables
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private RectTransform content;
+    [SerializeField] private List<GameObject> modules = new List<GameObject>();
+    [SerializeField] private List<int> waves = new List<int>();
+    
+    #endregion
+    
+    
+    #region Private Variables
+    #endregion
+
+    
+    #region MonoBeaviour
+    
     void Start()
     {
         
+        foreach (GameObject module in modules)
+        {
+            module.SetActive(false);
+        }
+
+        NextWave(10);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
+    #endregion
+    
+    
+    #region Public Methods
     public void Open()
     {
         animator.SetTrigger("open");
@@ -29,4 +58,25 @@ public class WavePreviewManager : MonoBehaviour
         animator.SetTrigger("close");
         
     }
+
+    public void NextWave(int wave)
+    {
+
+        for (int i = 0; i < modules.Count && i < waves.Count; i++)
+        {
+            if (waves[i] > wave)
+                break;
+            
+            content.sizeDelta = new Vector2(600, (moduleSize + 5) * (i + 1) + 10); 
+            
+            modules[i].SetActive(true);
+            Debug.Log("before : " + modules[i].transform.localPosition);
+            modules[i].transform.localPosition = new Vector3(modules[i].transform.localPosition.x, offset - moduleSize * i , modules[i].transform.localPosition.z);
+            Debug.Log("after : " + modules[i].transform.localPosition);
+        }
+        
+    }
+
+    #endregion
+    
 }
